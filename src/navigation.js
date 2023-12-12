09adc439f1b1e51e460af74c7894681a28fcf9b5
Kachi-Okorie <onyedikachi.okorie@yahoo.com>
2023-12-12 21:42:01 +1100

    Added CHloe Hulmes detail to Employee Recognition Page

--- src/navigation.js
+++ src/navigation.js
@@ -5,24 +5,6 @@ export const headerData = {
     {
       text: 'Home',
       href: getPermalink('/'),
-      // links: [
-      //   {
-      //     text: 'SaaS',
-      //     href: getPermalink('/homes/saas'),
-      //   },
-      //   {
-      //     text: 'Startup',
-      //     href: getPermalink('/homes/startup'),
-      //   },
-      //   {
-      //     text: 'Mobile App',
-      //     href: getPermalink('/homes/mobile-app'),
-      //   },
-      //   {
-      //     text: 'Personal',
-      //     href: getPermalink('/homes/personal'),
-      //   },
-      // ],
     },
     {
     text: 'Products',
